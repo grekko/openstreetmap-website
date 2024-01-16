63ea752d5d14dfc0c87f03036b8557254d770133
Gregory Igelmund <github.com@grekko.de>
2024-01-15 22:40:56 -0500

    Add rake task

--- db/migrate/20240107144209_add_author_and_body_to_notes.rb
+++ db/migrate/20240107144209_add_author_and_body_to_notes.rb
@@ -7,23 +7,25 @@ class AddAuthorAndBodyToNotes < ActiveRecord::Migration[7.1]
         t.column :body, :text, :null => true
       end
 
-      reversible do |dir|
-        dir.up do
-          execute <<-SQL.squish
-          UPDATE
-            notes
-          SET
-            body = c.body,
-            author_id = c.author_id,
-            author_ip = c.author_ip
-          FROM
-            note_comments c
-          WHERE
-            notes.id = c.note_id
-            AND c.event = 'opened';
-          SQL
-        end
-      end
+      # TODO: Should this happen in a migration or within e.g. a rake task in
+      # which we can perform some sanity checks and which runs in the background.
+      # reversible do |dir|
+      #   dir.up do
+      #     execute <<-SQL.squish
+      #     UPDATE
+      #       notes
+      #     SET
+      #       body = c.body,
+      #       author_id = c.author_id,
+      #       author_ip = c.author_ip
+      #     FROM
+      #       note_comments c
+      #     WHERE
+      #       notes.id = c.note_id
+      #       AND c.event = 'opened';
+      #     SQL
+      #   end
+      # end
     end
 
     add_foreign_key :notes, :users, :column => :author_id, :validate => false
